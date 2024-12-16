5e187e1f0bf903118f97ad690c5bd66b425bfa8f
friscasavira <friscasavira724gmail.com>
2024-12-16 10:23:48 +0700

    membuat auth login controller dan route admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Auth\AdminLoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+route::middleware(['guest'])->group(function () {
+    route::get('/admin/login', [AdminLoginController::class, 'login'])->name('admin.login');
+    route::post('/admin/login', [AdminLoginController::class, 'auth'])->name('admin.auth');
+});
+
+route::middleware(['admin'])->group(function () {
+    route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    route::get('/admin/profile', [AdminController::class, 'profile'])->name('admin.profile');
+    route::put('/admin/profile/update', [AdminController::class, 'update'])->name('admin.profile.update');
+    route::get('/admin/logout', [AdminController::class, 'logout'])->name('admin.logout');
 });
