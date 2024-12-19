d383c21e7b397a1a5848669a5208e3bcc17d7c59
friscasavira <friscasavira724gmail.com>
2024-12-19 12:46:26 +0700

    home

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -267,13 +267,13 @@
 
                     <div class="mb-4" data-aos="fade-up" data-aos-delay="200">
                         <iframe style="border:0; width: 100%; height: 270px;"
-                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus"
+                            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d31790.43720203043!2d105.3236314!3d-5.1351351!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sid!2sid!4v1734586532614!5m2!1sid!2sid"
                             frameborder="0" allowfullscreen="" loading="lazy"
                             referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </div><!-- End Google Maps -->
+
+                    </div><!-- End Google Maps https://maps.app.goo.gl/oB5mQreqZ7bWsjV57 -->
 
                     <div class="row gy-4">
-
                         <div class="col-lg-4">
                             <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="300">
                                 <i class="bi bi-geo-alt flex-shrink-0"></i>
@@ -310,50 +310,6 @@
                                     @endif
                                 </div>
                             </div><!-- End Info Item -->
-
-                        </div>
-
-                        <div class="col-lg-8">
-                            <form action="forms/contact.php" method="post" class="php-email-form"
-                                data-aos="fade-up" data-aos-delay="200">
-                                <div class="row gy-4">
-
-                                    <div class="col-md-6">
-                                        <input type="text" name="name" class="form-control"
-                                            placeholder="Your Name" required="">
-                                    </div>
-
-                                    <div class="col-md-6 ">
-                                        <input type="email" class="form-control" name="email"
-                                            placeholder="Your Email" required="">
-                                    </div>
-
-                                    <div class="col-md-12">
-                                        <input type="text" class="form-control" name="subject"
-                                            placeholder="Subject" required="">
-                                    </div>
-
-                                    <div class="col-md-12">
-                                        <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
-                                    </div>
-
-                                    <div class="col-md-12 text-center">
-                                        <div class="loading">Loading</div>
-                                        <div class="error-message"></div>
-                                        <div class="sent-message">Your message has been sent. Thank you!</div>
-
-                                        <button type="submit">Send Message</button>
-                                    </div>
-
-                                </div>
-                            </form>
-                        </div><!-- End Contact Form -->
-
-                    </div>
-
-                </div>
-
-            </section><!-- /Contact Section -->
 
     </main>
 
