a5a7dc7006312c582ec3e310d8ac7fe1ca1ed1fc
friscasavira <friscasavira724gmail.com>
2025-01-09 14:26:24 +0700

    .

--- resources/views/Admin/layouts/app.blade.php
+++ resources/views/Admin/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
-    <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/admin.jpeg') }}" />
+    <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/company.png') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/styles.min.css') }}" />
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
